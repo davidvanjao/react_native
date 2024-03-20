852f8ca2755e1424927746c93f290700c29cac6c
David Vanjão <35746587+davidvanjao@users.noreply.github.com>
2024-03-20 20:40:22 -0300

    ok

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 //import Props from './src/atividade_1';
 //import Exemplo_3 from './src/ex_3';
-import Atividade_3 from './src/atividade_3';
+//import Atividade_3 from './src/atividade_3';
+import Exemplo_4 from './src/ex_4';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade_3/>
+      <Exemplo_4/>
     </View>
   );
 }
---
+++ src/ex_4/index.js
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import styles from "./styles";
+
+export default function Exemplo_4() {
+    //const [numero, setNumero] = useState(0);
+  
+    return (
+    <View style={styles.container}>
+        <Text style={styles.titulo}>Exemplo 4</Text> 
+    </View>
+    );
+}

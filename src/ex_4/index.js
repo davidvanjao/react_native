c3acd6e33af3043499e30f01169190e04a001741
David Vanjão <35746587+davidvanjao@users.noreply.github.com>
2024-03-22 20:40:00 -0300

    0K

--- src/ex_4/index.js
+++ src/ex_4/index.js
@@ -1,13 +1,37 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Pressable } from "react-native";
 import React, { useState } from "react";
 import styles from "./styles";
+import { TextInput } from "react-native-web";
 
 export default function Exemplo_4() {
-    //const [numero, setNumero] = useState(0);
+    const [mensagem, setMensagem] = useState('MENSAGEM 1');
+    const [mensagem2, setMensagem2] = useState('MENSAGEM 2');
+    const [mensagemTela, setMensagemTela] = useState('');
+
   
     return (
     <View style={styles.container}>
         <Text style={styles.titulo}>Exemplo 4</Text> 
+
+        <Text style={styles.txtSaida}>{mensagemTela}</Text> 
+
+        <TextInput 
+            style={styles.txtEntrada}
+            placeholder=''
+            inChangeText={(txt) => setMensagem(txt)}
+        />
+
+        <Text style={styles.txtSaida}>{mensagemTela}</Text> 
+
+        <TextInput 
+            style={styles.txtEntrada}
+            placeholder=''
+            inChangeText={(txt2) => setMensagem2(txt2)}
+        />
+
+        <Pressable onPress={()=> setMensagemTela(mensagem2)}>
+            <Text>Exibir mensagem</Text> 
+        </Pressable>
     </View>
     );
 }
--- src/ex_4/styles.js
+++ src/ex_4/styles.js
@@ -15,6 +15,21 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#b71c1c'
     },
+    txtSaida: {
+        margin: 20,
+        fontSize: 25,
+        color:'#e53935',
+    },
+    txtEntrada: {
+        borderWidth: 3,
+        textAlign: 'center',
+        fontSize: 22,
+        borderColor: '#b71c1c',
+        height: 40,
+        width: '70%',
+        color: '#e53935'
+
+    }
 
   });
 
